5d605f03f4121a22049f204235792a415f28b715
孔祥瑞 <44524351+Xiangrui-Kong@users.noreply.github.com>
2021-03-12 00:54:31 +0800

    Create Trans.java

---
+++ 2021.3.12 Java/1 编写Java程序，将所有整型数值全部转换成int类型，并将转换后的值输出。（上传源程序、运行结果和自己对程序的讲解视频。）/Trans.java
@@ -0,0 +1,17 @@
+/**
+ * Trans
+ */
+public class Trans {
+
+    public static void main(String[] args) {
+        short num1=1;
+        byte num2=2;
+        int num3=3;
+        long num4=4;
+        int a=(int)num1;
+        int b=(int)num2;
+        int c=(int)num3;
+        int d=(int)num4;
+        System.out.printf("%d %d %d %d",a,b,c,d);
+    }
+}
