a3e0d9ad26697d37d424086b3a0cbf0eca45c197
孔祥瑞 <44524351+Xiangrui-Kong@users.noreply.github.com>
2021-04-01 17:47:08 +0800

    Create test.java

---
+++ 2021.4.1 Java/1 编写程序将两个数相加的结果输出。（要求上传源程序，运行结果）/test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        Scanner x=new Scanner(System.in);
+        float a,b;
+        a=x.nextFloat();
+        b=x.nextFloat();
+        float c=a+b;
+        System.out.println(c);
+    }
+}
